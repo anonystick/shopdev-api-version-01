5c87d30200bf9db753c7ce2886635a463db9f2a9
Anonystick <tungns@queenb.vn>
2023-11-04 15:14:46 +0700

    FEAT: add aoi user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,25 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/api/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success api',
+        message: 'api ok',
+        metadata: [
+            {
+                name: 'anonystick',
+                age: 40
+            },
+            {
+                name: 'Ronaldo',
+                age: 39
+            },
+            {
+                name: 'Messi',
+                age: 37
+            }
+        ]
+    })
+})
+
 module.exports = router;
